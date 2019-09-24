c168fe7b74a403e775e70cde0d5bbd5408a8be7e
abrahamjuliot <abrahamjuliot@gmail.com>
2019-09-24 14:21:21 -0700

    added photo and site

--- data/ensc.js
+++ data/ensc.js
@@ -127,12 +127,12 @@ export default [
   },
   {
     "name": "Jenna Wiegand",
-    "img": "",
+    "img": "jenna",
     "email": "jwieg002@",
     "program": "ENSC",
     "faculty": "volz",
-    "research": "",
-    "websiteURL": ""
+    "research": "Developmental toxicity of organophosphate flame retardants",
+    "websiteURL": "https://www.researchgate.net/profile/Jenna_Wiegand"
   },
   {
     "name": "Alexa Canchola",
